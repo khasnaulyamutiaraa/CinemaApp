199736252f4377565bcb2b45e6a13b6f3305cd9d
Khasna Ulya Mutiara <khasna20670093@upgris.ac.id>
2023-01-13 09:39:16 +0700

    Initial commit

---
+++ app/src/main/java/com/lazday/youtubeplayer/retrofit/Constant.java
@@ -0,0 +1,17 @@
+package com.lazday.youtubeplayer.retrofit;
+
+public class Constant {
+
+    public static final String KEY              = "88fcca505cc978edfe0b4ff7de16d039";
+    public static final String BASE_URL         = "https://api.themoviedb.org/3/";
+    public static final String BACKDROP_PATH    = "https://image.tmdb.org/t/p/w500_and_h282_face";
+    public static final String POSTER_PATH      = "https://image.tmdb.org/t/p/w220_and_h330_face";
+
+    public static final String LANGUAGE         = "en-US";
+
+    public static final String POPULAR          = "POPULAR";
+    public static final String NOW_PLAYING      = "NOW_PLAYING";
+
+    public static String MOVIE_ID       = null;
+    public static String MOVIE_TITLE    = null;
+}
